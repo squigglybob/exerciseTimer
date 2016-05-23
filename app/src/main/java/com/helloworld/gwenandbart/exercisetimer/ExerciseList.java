b8a95ea5ad85143ced714cf91262e70cc5e918f2
Nath Fenton <nathinabob@gmail.com>
2016-05-23 05:40:46 +0100

    Initial Commit

---
+++ app/src/main/java/com/helloworld/gwenandbart/exercisetimer/ExerciseList.java
@@ -0,0 +1,13 @@
+package com.helloworld.gwenandbart.exercisetimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExerciseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_list);
+    }
+}
