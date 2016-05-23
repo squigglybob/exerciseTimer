106916b8d8a40da7c3297a0da676bcdc9772dfb0
Nath Fenton <nathinabob@gmail.com>
2016-05-23 09:36:39 +0100

    JSON locked and loaded. Closes #1

--- app/src/main/java/com/helloworld/gwenandbart/exercisetimer/ExerciseList.java
+++ app/src/main/java/com/helloworld/gwenandbart/exercisetimer/ExerciseList.java
@@ -1,13 +1,65 @@
 package com.helloworld.gwenandbart.exercisetimer;
 
+import android.content.res.Resources;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class ExerciseList extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseList extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_list);
+
+        InputStream rawAppData = getResources().openRawResource(R.raw.appdata);
+        Log.d("exTime", rawAppData.toString());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(rawAppData));
+        String appData = loadJSONFromAsset();
+        Log.d("exTime", "json is "+appData);
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getResources().openRawResource(R.raw.appdata);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+            return json;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
+
